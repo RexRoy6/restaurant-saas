2531f9a28d586c06b51c032e7e470499df5226b2
Rodrigo Aguilera <rodris9806@gmail.com>
2026-09-24 13:19:14 -0600

    refactor: remove legacy CRM company pages

--- src/app/company/clients/page.tsx
+++
@@ -1,9 +0,0 @@
-import ClientsModule from "@/app/components/crm/clients/ClientsModule";
-import { requireRole } from "@/lib/auth/requireRole";
-
-export default async function ClientsPage() {
-
-  await requireRole(["owner","employee"]);
-
-  return <ClientsModule />;
-}
